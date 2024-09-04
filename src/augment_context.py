6c03e0b80752ecd0a6d10c1e14d6727344d77c45
Phan Van Hoang <phanhoang1803@gmail.com>
2024-09-04 20:44:55 +0700

    Modify prompt

--- src/augment_context.py
+++ src/augment_context.py
@@ -41,9 +41,8 @@ Given the the image and the captions associated with it, you need to write a det
 1. Describes the key visual elements and content of the image.
 2. Infers the most likely context, setting, or event depicted from the captions. Incorporate relevant details from the captions to enhance the description.
 3. Estimates the time period and possible geographic location, if possible.
-4. Identifies any significant themes or messages conveyed by the image, integrating any insights from the captions, if possible.
 
-You have to write the context in a single paragraph of 10 - 15 sentences. Be specific and confident in your description and inferences. Do not mention limitations in identifying specific individuals or places.
+You have to write the context in a single paragraph of 7-10 sentences. Be specific and confident in your description and inferences. Do not mention limitations in identifying specific individuals or places.
 
 Context:"""
 
